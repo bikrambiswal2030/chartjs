ea603e6a92e8188c874d332b1d290af2cb2b6b66
Bikram <bikrambiswal2030@gmail.com>
2020-03-05 12:43:35 +0530

    check string is palindrome or not

--- Script/main1.js
+++ Script/main1.js
@@ -1,11 +1,11 @@
 function palindrome(){
-revString= "";
-inpString= document.getElementById("text").value;
-i=inpString.length;
+rev= "";
+input= document.getElementById("text").value;
+i=input.length;
 for(var j=i; j>=0; j--){
-    revString = revString+inpString.charAt(j);
+    rev = rev+input.charAt(j);
 }
-if(revString == inpString)
+if(rev == input)
         document.getElementById("result").innerHTML = "String is a pallindrome";
     else
         document.getElementById("result").innerHTML = "String is not a pallindrome";
