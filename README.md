4e4e3fbdb4a68ecfd42d90f1995854c2883aeef5
Bikram <bikrambiswal2030@gmail.com>
2020-04-04 20:20:31 +0530

    bulid library using node and npm

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Graph.js
-
-I am Anil Kumar Sahoo who made this Graph Library.....
-
-Using this library you can make different charts.
--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,13 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-
 export default {
   input: "src/index.js",
   output: [
     {
-      name: "Graph",
-      file: "dist/graph.js",
-      format: "umd",
-    },
-    {
-      name: "GraphMin",
-      file: "dist/graph.min.js",
-      format: "umd",
-      plugins: [terser()],
-    },
+      name: "charts",
+      file: "build/chart.js",
+      format: "umd"
+    }
   ],
-  plugins: [resolve(), commonjs()],
+  plugins: [resolve(), commonjs()]
 };
